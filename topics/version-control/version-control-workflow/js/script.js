c8a1a4fa642b887bfaa7884700453c1ddddf3b0a
iliannaf <iliannafaoro@gmail.com>
2026-09-10 16:58:58 -0400

    Colours and Sizes
    Changing the colours and sizes of the shapes.

--- topics/version-control/version-control-workflow/js/script.js
+++ topics/version-control/version-control-workflow/js/script.js
@@ -14,7 +14,7 @@
 */
 function setup() {
     // A 640x480 canvas
-    createCanvas(640, 640);
+    createCanvas(1200, 960);
 
     // Don't show the cursor
     noCursor();
@@ -26,11 +26,11 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    background(51, 101, 158);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 10;
+    const levels = 16;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -42,14 +42,14 @@ function draw() {
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
-        fill(shade, shade, 0);
+        fill(shade, 34, shade);
         // Draw rectangles from the centre
         rectMode(CENTER);
         // Draw the rectangle in the centre of the canvas
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(320, 320, level * 48, level * 48);
+        rect(500, 600, level * 32, level * 24);
         pop();
     }
 
@@ -58,8 +58,8 @@ function draw() {
     // No line around the shape
     noStroke();
     // Make it red (RGB)
-    fill(255, 0, 0);
+    fill(73, 42, 90);
     // Draw a 100x100 circle at the mouse position
-    ellipse(mouseX, mouseY, 100, 100);
+    ellipse(mouseX, mouseY, 80, 48);
     pop();
 }
