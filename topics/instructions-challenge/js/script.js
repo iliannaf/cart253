017b517557fa1967697f538054c7ede1719d196f
iliannaf <iliannafaoro@gmail.com>
2026-09-17 16:28:10 -0400

    Landscape challenge
    Adding the landscape challenge that I worked on with Mathew

---
+++ topics/instructions-challenge/js/script.js
@@ -0,0 +1,69 @@
+/**
+ * Challenge Landscape
+ * Matthew Thompson and Ilianna Faoro
+ * 
+ * Draw a Landscape Challenge for week 2 CART 253
+ */
+
+"use strict";
+
+/**
+ Draw a Canvas 720p
+*/
+function setup() {
+    createCanvas(1280, 720);
+
+}
+
+
+/**
+ Draw a Mountain Landscape (Cuz I'm from Alberta!!)
+*/
+function draw() {
+    //Draw the Sky
+    background("#7cd3f2");
+
+    //Draw the Horizon 
+    push();
+    //Huge circle, thick green stroke, light green fill
+    stroke("#063d0a");
+    strokeWeight(15);
+    fill("#54c75b");
+   //place a huge circle sitting under the canvas to be the horizon
+    ellipse(640, 1720, 2400);
+    pop();
+    
+    //Draw a smaller mountain to sit behind 
+    push();
+    stroke("black")
+    strokeWeight(3);
+    fill("grey");
+    // Draw a smaller triangle 
+    triangle(200, 720, 500, 400, 600, 720);
+    pop();
+    //Draw a Mountain in front on the left
+    push();
+    stroke("black")
+    strokeWeight(3);
+    fill("grey");
+    // Draw a triangle that is sitting at the bottom of the canvas and goes off the canvas on the left
+    triangle(-100, 720, 200, 360, 500, 720);
+    pop();
+
+    //Add a Snowcap
+    push();
+    noStroke();
+    fill("white");
+    //MATH!?! Heck no, we guess an check like MADMEN.
+    triangle(117, 460, 200, 360, 300, 480);
+    pop();
+
+    //Lets add some SUN
+    push();
+    noStroke();
+    fill("#d14521");
+    //Place the sun top right off screen
+    ellipse(1280, 0, 500);
+    pop();
+
+}
