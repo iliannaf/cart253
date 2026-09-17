ac607d47c3ac47372942600f4169cc13f736c32b
iliannaf <iliannafaoro@gmail.com>
2026-09-17 13:53:29 -0400

    Comments
    Writing comments

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Ilianna Faoro
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Description: Week 2 stuff
  */
 
 "use strict";
