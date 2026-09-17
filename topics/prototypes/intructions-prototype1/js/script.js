03f0491da00e941c6e341655f0effc4733946867
iliannaf <iliannafaoro@gmail.com>
2026-09-17 17:24:18 -0400

    Draw the Building
    Draws the building, windows and door.

--- topics/prototypes/intructions-prototype1/js/script.js
+++ topics/prototypes/intructions-prototype1/js/script.js
@@ -1,15 +1,14 @@
 /**
- * Title of Project
+ * Building
  * Ilianna
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A building on a field of grass, because that makes sense in this world.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Cretaes a canvas to work on.
 */
 function setup() {
     createCanvas(800, 600);
@@ -17,7 +16,7 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Creates a building with windows and a door.
 */
 function draw() {
     background("lightblue");
@@ -35,5 +34,35 @@ function draw() {
     strokeWeight(2);
     fill("#8c8d91");
     rect(250, 70, 300, 500)
+    pop();
+
+    // Add windows
+    push();
+    stroke("black");
+    strokeWeight(2);
+    fill("#90b7f5");
+    rect(300, 120, 50);
+    rect(450, 120, 50);
+    rect(300, 220, 50);
+    rect(450, 220, 50);
+    rect(300, 320, 50);
+    rect(450, 320, 50);
+    rect(300, 420, 50);
+    rect(450, 420, 50);
+    pop();
+
+    // Add a door
+    push();
+    stroke("black");
+    strokeWeight(2);
+    fill("#6e5347");
+    rect(375, 490, 50, 80);
+    pop();
+
+    // Add a door handle bar
+    push();
+    stroke("black");
+    strokeWeight(5);
+    line(390, 540, 410, 540);
     pop();
 }
