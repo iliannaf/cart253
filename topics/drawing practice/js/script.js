e6acf548a8fcf9c7c01c7dca91c39199d9930969
iliannaf <iliannafaoro@gmail.com>
2026-09-17 14:06:55 -0400

    Drawing a Record
    Using ellipses to create a record.

--- topics/drawing practice/js/script.js
+++ topics/drawing practice/js/script.js
@@ -9,16 +9,44 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
-function setup() {
 
+function setup() {
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Adding a background
+ * Adding an ellipse
+ * Colour the border and inside of the ellipse
+ * Use shapes to create a record
 */
 function draw() {
+    background(150, 150, 150);
 
+    push();
+
+    fill(255, 0, 0);
+    stroke(255, 255, 255);
+    ellipse(320, 320, 480, 480);
+
+    pop();
+
+    push();
+
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(320, 320, 140, 140);
+
+    pop();
+
+    push();
+
+    fill(150, 150, 150);
+    stroke(50, 50, 50);
+    ellipse(320, 320, 20, 20);
+
+    pop();
 }
