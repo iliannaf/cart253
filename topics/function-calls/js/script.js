04106722a87ec22f076aa26ed374f4831956cf5f
iliannaf <iliannafaoro@gmail.com>
2026-09-17 13:49:57 -0400

    Function Calls
    Starting to do P5

---
+++ topics/function-calls/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * Creates the Canvas
+*/
+function setup() {
+    createCanvas(640, 480);
+}
+
+
+/**
+ * Colours the background
+ * Creates a rectangle
+*/
+function draw() {
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
+}
