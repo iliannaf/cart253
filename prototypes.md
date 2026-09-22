acdfb3b25d9b64af68b9b86ffa913c04dbb333bc
iliannaf <iliannafaoro@gmail.com>
2026-09-22 10:23:08 -0400

    Finished Diglet
    Finished drawing Diglet and added the link to my website.

--- prototypes.md
+++ prototypes.md
@@ -3,3 +3,5 @@
 ## Drawings - Week 2
 
 ### [Prototype 1 - Standard Building](topics/prototypes/intructions-prototype1/index.html)
+
+### [Prototype 2 - Diglet](topics/prototypes/intructions-prototype2/index.html)
--- topics/prototypes/intructions-prototype2/js/script.js
+++ topics/prototypes/intructions-prototype2/js/script.js
@@ -24,21 +24,56 @@ function draw() {
     // Draw the head
     push ();
     noStroke();
-    fill(158, 113, 74);
+    fill(161, 133, 109);
     ellipse(400, 300, 200);
     pop ();
 
     // Draw the body
     push();
     noStroke();
-    fill(158, 113, 74);
-    rect(300, 300, 200, 200,);
+    fill(161, 133, 109);
+    rect(300, 300, 200, 200);
+    pop();
+
+    // Draw the nose
+    push();
+    noStroke();
+    fill(224, 144, 211);
+    ellipse(380, 330, 80, 50);
+    pop();
+
+    // Draw the eyes
+    push();
+    noStroke();
+    fill(77, 75, 76);
+    ellipse(360, 270, 20, 40);
+    ellipse(410, 270, 20, 40);
+    pop();
+
+    // Finish drawing the eyes
+    push();
+    noStroke();
+    fill(181, 181, 181);
+    ellipse(360, 260, 10, 10);
+    ellipse(410, 260, 10, 10);
     pop();
 
     // Draw the rocks
     push();
     noStroke();
     fill(138, 130, 123);
-    ellipse(500, 500, 30, 35);
+    ellipse(500, 500, 40, 45);
+    ellipse(480, 500, 45, 50);
+    ellipse(470, 500, 40, 45);
+    ellipse(450, 500, 40, 45);
+    ellipse(440, 500, 40, 45);
+    ellipse(420, 500, 45, 50);
+    ellipse(400, 500, 40, 45);
+    ellipse(390, 500, 40, 45);
+    ellipse(370, 500, 45, 50);
+    ellipse(350, 500, 40, 45);
+    ellipse(340, 500, 40, 45);
+    ellipse(320, 500, 45, 50);
+    ellipse(300, 500, 40, 45);
     pop();
 }
