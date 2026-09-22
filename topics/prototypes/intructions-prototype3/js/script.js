b0455bd0e66b8ae94a82ebb4dbdfdd4160b3c340
iliannaf <iliannafaoro@gmail.com>
2026-09-22 10:34:33 -0400

    Start 3rd prototype
    Start creating the 3rd prototype.

--- topics/prototypes/intructions-prototype3/js/script.js
+++ topics/prototypes/intructions-prototype3/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Title of Project
- * Author Name
+ * Ilianna
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -12,7 +12,9 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(800, 600);
+    let shapeColour;
+    let shapePosition;
 }
 
 
@@ -20,5 +22,16 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(232, 232, 232);
+      shapeColour = color(random(0, 255), random(0, 255), random(0, 255));
+      shapeX = (random(0, 800));
+      shapeY = (random(0, 600));
+      shapeW = (random(0,40));
+      shapeH = (random(0,20));
 
+    push();
+    stroke(133, 133, 133);
+    fill(shapeColour);
+    rect();
+    pop();
 }
