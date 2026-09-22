b80a70692746a91f06da6c1cce505c662b363a37
iliannaf <iliannafaoro@gmail.com>
2026-09-22 10:47:12 -0400

    Finish 3rd Prototype
    Finished the 3rd prototype. Create a bunch of random stuff.

--- prototypes.md
+++ prototypes.md
@@ -5,3 +5,5 @@
 ### [Prototype 1 - Standard Building](topics/prototypes/intructions-prototype1/index.html)
 
 ### [Prototype 2 - Diglet](topics/prototypes/intructions-prototype2/index.html)
+
+### [Prototype 3 - Randomness](topics/prototypes/intructions-prototype3/index.html)
--- topics/prototypes/intructions-prototype3/js/script.js
+++ topics/prototypes/intructions-prototype3/js/script.js
@@ -1,37 +1,40 @@
 /**
- * Title of Project
+ * Randomness
  * Ilianna
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Use variables to create a bunch of random shapes. Different positions, sizes and colours.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas to work on
 */
 function setup() {
     createCanvas(800, 600);
-    let shapeColour;
-    let shapePosition;
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw shapes with random sizes and colours, in random places
 */
 function draw() {
     background(232, 232, 232);
-      shapeColour = color(random(0, 255), random(0, 255), random(0, 255));
-      shapeX = (random(0, 800));
-      shapeY = (random(0, 600));
-      shapeW = (random(0,40));
-      shapeH = (random(0,20));
+    // Create variables for shape values
+      let shapeColour = color(random(0, 255), random(0, 255), random(0, 255));
+      let shapeX = (random(0, 800));
+      let shapeY = (random(0, 600));
+      let shapeW = (random(40,120));
+      let shapeH = (random(20,80));
 
     push();
     stroke(133, 133, 133);
     fill(shapeColour);
-    rect();
+    // Create random rectangles
+    rect(shapeX, shapeY, shapeW, shapeH);
+    // Change values a bit, to not make them all the same
+    ellipse((shapeX * 0.5), (shapeY * 0.5), (shapeW * 0.5), (shapeH * 0.5));
+    rect((shapeX * 0.7), (shapeY * 0.7), (shapeW * 0.7), (shapeH * 0.7));
+    ellipse((shapeX * 1.2), (shapeY * 1.2), (shapeW * 1.2), (shapeH * 1.2));
     pop();
 }
