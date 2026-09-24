86978b23c40b69e20ff3e39e25c3e0a3f407952f
iliannaf <iliannafaoro@gmail.com>
2026-09-23 21:37:21 -0400

    Add circle
    Circle that changes colours depending on mouse position.

--- topics/introduction-variables/js/script.js
+++ topics/introduction-variables/js/script.js
@@ -1,24 +1,29 @@
 /**
- * Title of Project
- * Author Name
+ * Circle with variables
+ * Ilianna
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Creates circle with variables
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create a canvas with a circle
 */
 function setup() {
-
+    // Create the canvas
+    createCanvas(640, 640);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    background(0);
+    
+    // Draw a circle that changes colour based on the mouse position
+    push();
+    noStroke();
+    // We use the variable names mouseX and mouseY instead
+    // of numbers for the red and green of the circle's fill
+    fill(mouseX, mouseY, 0);
+    ellipse(width/2, height/2, 100, 100);
+    pop();
 }
